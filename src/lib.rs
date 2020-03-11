803e80659593863a2ae722a354c1a3eded901dbd
SlightlyOutOfPhase <slightlyoutofphase@gmail.com>
2020-03-11 14:20:51 -0400

    Use the `len()` function from the input

--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,21 @@
 #![no_std]
 #![feature(const_fn, const_if_match, const_loop)]
 
-/// This macro takes the following parameters in the order they're listed:
-/// type to sort, index to start at, index to end at, total array length, and name of existing
-/// const / static array variable (or directly-passed "anonymous" array).
+/// This macro takes the following parameters in the order they're listed: type to sort, index to
+/// start at, index to end at, and either the name of an existing `const` or `static` array variable
+/// or just a directly-passed "anonymous" array.
 #[macro_export]
 macro_rules! staticsort {
-  ($type:ty, $low:expr, $high:expr, $len:expr, $values:expr) => {{
-    match $len {
+  ($type:ty, $low:expr, $high:expr, $values:expr) => {{
+    match $values.len() {
       0 => $values,
       _ => {
         #[inline]
         const fn static_sort(
-          mut values: [$type; $len],
+          mut values: [$type; $values.len()],
           mut low: isize,
           mut high: isize,
-        ) -> [$type; $len]
+        ) -> [$type; $values.len()]
         {
           if high - low <= 0 {
             return values;

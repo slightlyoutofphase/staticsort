68a0e71860a8d06f4141f9108ca621399d0b558b
SlightlyOutOfPhase <slightlyoutofphase@gmail.com>
2020-03-11 14:36:41 -0400

    The *input* can only be a `const` this way I guess, but it still seems better.

--- examples/main.rs
+++ examples/main.rs
@@ -2,9 +2,9 @@
 
 use staticsort::staticsort;
 
-static X: [usize; 12] = [1, 6, 2, 5, 3, 4, 7, 12, 8, 11, 9, 10];
+const X: [usize; 12] = [1, 6, 2, 5, 3, 4, 7, 12, 8, 11, 9, 10];
 
-static Y: [f64; 12] = [
+const Y: [f64; 12] = [
   1.0, 6.0, 2.0, 5.0,
   3.0, 4.0, 7.0, 12.0,
   8.0, 11.0, 9.0, 10.0,
@@ -12,8 +12,8 @@ static Y: [f64; 12] = [
 
 // The macro takes the following parameters in the order they're
 // listed: type to sort, index to start at, index to end at, and
-// either the name of an existing const or static array variable
-// or just a directly-passed "anonymous" array.
+// either the name of an existing const array variable or just
+// a directly-passed "anonymous" array.
 
 // Sort all of X:
 static XX: [usize; 12] = staticsort!(usize, 0, 11, X);
